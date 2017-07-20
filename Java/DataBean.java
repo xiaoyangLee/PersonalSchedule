35ca90bf781dd131d0e21acd9740142409598307
xiaoyang <lixiaoyang2020@gmail.com>
2017-07-20 08:04:09 +0800

    Add files via upload

---
+++ Java/DataBean.java
@@ -0,0 +1,40 @@
+package com.android.xiaoyang.personalschedule;
+
+/**
+ * Created by xiaoyang on 2017/6/21.
+ */
+
+public class DataBean {
+    private int id;
+    private String date;
+    private String schedule;
+
+    public DataBean(){
+
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(String schedule) {
+        this.schedule = schedule;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
